0760831eb6fe5c8b8eb08c04659d4b35af425f3d
Abdullah Abdulkareem <abdullah@Abdullahs-MacBook-Air-2.local>
2023-10-11 15:05:36 -0500

    on submit, login page is able to retrieve the email and password and store it in 2 variables

--- login-page/login.js
+++ login-page/login.js
@@ -1,2 +1,8 @@
 
-const button = document.querySelector("")
+function submitForm(event){
+    event.preventDefault()
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+}
