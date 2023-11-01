5f6eeb6617299c1d9cd72112adc2aef437c13610
Jace B <jace.e.blackwell-1@ou.edu>
2023-11-01 17:07:55 -0500

    Added spring boot framework and thymeleaf

---
+++ attendancehub/src/main/java/com/attendance/attendancehub/AttendancehubApplication.java
@@ -0,0 +1,13 @@
+package com.attendance.attendancehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendancehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendancehubApplication.class, args);
+	}
+
+}
---
+++ attendancehub/src/test/java/com/attendance/attendancehub/AttendancehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendance.attendancehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
