872ab9a6d65f83e306a7b584d9b3615d07ffeaa7
Ifesinachi Nwatu <Ifesinachi.F.Nwatu-1@ou.edu>
2023-11-11 06:19:26 -0600

    Modified controller - Added newStudentForm Method

--- attendancehub/src/main/java/com/attendance/attendancehub/controller/StudentController.java
+++ attendancehub/src/main/java/com/attendance/attendancehub/controller/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.attendance.attendancehub.model.Student;
 import com.attendance.attendancehub.service.StudentService;
 
 @Controller
@@ -19,6 +20,16 @@ public class StudentController {
 
         model.addAttribute("listStudents", studentService.getAllStudents());
         return "index";
+    }
+
+    //Enables Controller to handle add employee request
+    @GetMapping("/showNewStudentForm")
+    public String showNewStudentForm(Model model) {
+
+        //create model attribute to bind form data
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "new_employee";
     }
 
 }
