6ffb849c6f45cdb6749e6fb8838be3ccaeec7ae7
Ifesinachi Nwatu <Ifesinachi.F.Nwatu-1@ou.edu>
2023-11-11 07:48:06 -0600

    Changes made to StudentService and StudentServiceIMpl

--- attendancehub/src/main/java/com/attendance/attendancehub/service/StudentServiceImpl.java
+++ attendancehub/src/main/java/com/attendance/attendancehub/service/StudentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.attendance.attendancehub.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,23 @@ public class StudentServiceImpl implements StudentService{
     public void saveStudent(Student student) {
        
        this.studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(long id) {
+        
+        Optional<Student> optional = studentRepository.findById(id);
+        Student student = null;
+
+        if (optional.isPresent()){
+            student = optional.get();
+        }
+
+        else {
+
+            throw new RuntimeException("Student not found for id :: " + id);
+        }
+        return student;
     }
     
 }
