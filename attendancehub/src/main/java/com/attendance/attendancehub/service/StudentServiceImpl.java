d9970ab2e8bca3ea5fdfaace44ee6b2fdbb985a2
Ifesinachi Nwatu <Ifesinachi.F.Nwatu-1@ou.edu>
2023-11-11 04:00:38 -0600

    Modified StudentServiceImpl

--- attendancehub/src/main/java/com/attendance/attendancehub/service/StudentServiceImpl.java
+++ attendancehub/src/main/java/com/attendance/attendancehub/service/StudentServiceImpl.java
@@ -3,10 +3,12 @@ package com.attendance.attendancehub.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.attendance.attendancehub.model.Student;
 import com.attendance.attendancehub.repository.StudentRepository;
 
+@Service
 public class StudentServiceImpl implements StudentService{
 
     @Autowired
