2610e1bc1504902aa99ce64cee80b31fa24ed90c
Jace B <jace.e.blackwell-1@ou.edu>
2023-11-11 20:48:13 -0600

    Update application properties

---
+++ attendancehub/src/test/java/com/attendancehub/attendancehub/AttendancehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendancehub.attendancehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
