a76e1552455fa31bc4690c2170eac8c9bfb037b3
Jace B <jace.e.blackwell-1@ou.edu>
2023-11-24 08:45:17 -0600

    Finished backend for login

--- attendancehub/src/main/java/com/attendancehub/attendancehub/Impl/serviceimpl.java
+++ attendancehub/src/main/java/com/attendancehub/attendancehub/Impl/serviceimpl.java
@@ -1,10 +1,15 @@
 package com.attendancehub.attendancehub.Impl;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.attendancehub.attendancehub.Role;
@@ -19,7 +24,10 @@ public class serviceimpl implements service
 
     @Autowired
     private UserRepository userRepository;
-    
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public serviceimpl(UserRepository userRepository) 
     {
         this.userRepository = userRepository;
@@ -28,14 +36,26 @@ public class serviceimpl implements service
     @Override
     public User save(RegistrationDto registrationDto) 
     {
-        User user = new User(registrationDto.getFirstName(), registrationDto.getLastName(), registrationDto.getEmail(), registrationDto.getPassword(), Arrays.asList(new Role("ROLE_USER")));
+        User user = new User(registrationDto.getFirstName(), registrationDto.getLastName(), registrationDto.getEmail(), passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
         return userRepository.save(user);
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException 
+    {
+        User user = userRepository.findByEmail(username);
+        if (user == null)
+        {
+            throw new UsernameNotFoundException("Invalid username/password");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
     }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles)
+    {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
 
 
     
---
+++ attendancehub/src/main/java/com/attendancehub/attendancehub/web/maincontroller.java
@@ -0,0 +1,13 @@
+package com.attendancehub.attendancehub.web;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class maincontroller 
+{
+    @GetMapping("/login")
+    public String login()
+    { 
+        return "login";
+    }
+}
