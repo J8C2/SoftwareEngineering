2619f0beaeb19aaff51e3ec2276abf38b1aeeecd
Abdullah Abdulkareem <abdullah@Abdullahs-Air-2.attlocal.net>
2023-11-24 11:52:26 -0600

    added test for loading up the home page

--- attendancehub/src/test/java/com/attendance/attendancehub/controller/EventControllerTest.java
+++ attendancehub/src/test/java/com/attendance/attendancehub/controller/EventControllerTest.java
@@ -1,15 +1,28 @@
 package com.attendance.attendancehub.controller;
 
+import com.attendance.attendancehub.service.EventService;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 
 
 @ExtendWith(SpringExtension.class)
@@ -21,6 +34,10 @@ class EventControllerTest {
 
     @Test
     void viewHomePage() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/");
+        MvcResult result = mvc.perform(request).andReturn();
+        int statusCode = result.getResponse().getStatus();
+        assertEquals(200, statusCode);
     }
 
     @Test
